01d8cf6acf573f4889ae3180d2858d4b5a75bada
BillieEyelash <riatalwar@hotmail.com>
2023-02-23 21:10:31 -0800

    Formatting/naming modifications

--- CreatePCoA.R
+++ CreatePCoA.R
@@ -12,21 +12,21 @@ envData = read.csv("C:/Users/riata/Documents/src/Polygence-Research-Project/Poly
 ma = as.matrix(ibs)
 
 # create hierarchical clustering based on distance matrix
-hc=hclust(as.dist(ma),"ave")
+hc = hclust(as.dist(ma),"ave")
 plot(hc)
 
 pheatmap(ma)
 
 # Plot in PCA space 
 # Capscale creates a dbRDA - distance-based-RDA, this is similar to PCoA - object 
-pp0=capscale(ma~1)
+pp0 = capscale(ma~1)
 plot(pp0)
 
 # get the scores from the object to plot in ggplot 
 score_dist <- as.data.frame(pp0$CA$u)
 score_dist$ID = rownames(score_dist)
 
-metaenv <- left_join(meta, envData, by=c("Origin"="Location.Name")) # merge environmental data with metadata by location
+metaenv <- left_join(meta, envData, by=c("Origin"="Location")) # merge environmental data with metadata by location
 score_meta <- left_join(score_dist, metaenv) # merge meta/environmental data on organism ID
 
 # Color graph with gradient based on env variable (change var using color)
@@ -38,8 +38,17 @@ ggplot(score_meta, aes(x=MDS1, y =MDS2, color = Origin)) +
 
 # Create RDA for specific variables
 ma.sub = ma[rownames(ma) %in% metaenv$ID, colnames(ma) %in% metaenv$ID]
-rda=capscale(ma.sub~Avg..Sea.Surface....C.+Salinity..ppt., metaenv)
+rda=capscale(ma.sub~Genetic.Cluster, metaenv)
 plot(rda)
+# LIST OF VARIABLES
+# Genetic.Cluster
+# Current.Direction
+# Current.Speed
+# Current.Stability
+# Polybrominated.Diphenyl.Ethers
+# Polychlorinated.Biphenyls
+# Avg.Sea.Surface.Temp
+# Salinity
 
 # Get coordinates of points on plot
 coords <- data.frame(score_meta$ID, score_meta$MDS1, score_meta$MDS2)
