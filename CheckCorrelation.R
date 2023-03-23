1051fe029f7b7ccdf93352dce7da2ec5f59e6794
BillieEyelash <riatalwar@hotmail.com>
2023-03-22 22:08:31 -0700

    Get variable correlation

---
+++ CheckCorrelation.R
@@ -0,0 +1,28 @@
+library(vegan)
+library(tidyverse)
+
+# load all data
+setwd("C:/Users/riata/Documents/src/Polygence-Research-Project/") # set directory to file location
+ibs = read.csv("C:/Users/riata/Documents/src/Polygence-Research-Project/Polygence-Data/DistanceMatrix.csv", row=1) # get distance matrix data
+nam = read.table("C:/Users/riata/Documents/src/Polygence-Research-Project/Polygence-Data/DolphinNames") # get dolphin name data
+meta = read.csv("C:/Users/riata/Documents/src/Polygence-Research-Project/Polygence-Data/Metadata.csv", fill=TRUE) # get metadata
+envCSV = read.csv("C:/Users/riata/Documents/src/Polygence-Research-Project/Polygence-Data/EnvironmentalData.csv", fill=TRUE, row=1) # get environmental data
+
+ma = as.matrix(ibs)
+envMa = as.matrix(envCSV)
+# standardize data
+envStand = decostand(envMa, "range")
+envDF = as.data.frame(envStand)
+
+data <- left_join(meta, rownames_to_column(envDF), by=c("Origin"="rowname"), copy=TRUE) # merge environmental data with metadata by location
+
+# check if variables are correlated with each other
+cor(data$Avg.Sea.Surface.Temp, data$Salinity)
+# Genetic.Cluster
+# Current.Direction
+# Current.Speed
+# Current.Stability
+# Polybrominated.Diphenyl.Ethers
+# Polychlorinated.Biphenyls
+# Avg.Sea.Surface.Temp
+# Salinity
